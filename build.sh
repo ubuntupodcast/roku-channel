c1414be0a8aa08bc9d15b8b0e03454600d3146ff
Martin Wimpress <code@flexion.org>
2016-08-08 14:27:11 +0100

    Add bogus assets.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-zip -r ubuntupodcast.zip . -x *.git* -x build.sh -x README.md -x LICENSE
+zip -r ubuntupodcast.zip . -x *.git* -x build.sh -x README.md -x LICENSE -x bogus_assets
