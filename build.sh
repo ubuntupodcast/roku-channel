e7bbc49f53aa1cd9c4c096c36d6bda8823c2b9f7
Martin Wimpress <code@flexion.org>
2016-08-09 11:12:00 +0100

    Update build script.

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-zip -r ubuntupodcast.zip . -x *.git* -x build.sh -x README.md -x LICENSE -x bogus_assets
+app="ubuntupodcast"
+
+# The manifest is not really shell, but this "works".
+source ./manifest 2>/dev/null
+
+# Zip the bundle.
+zip -r ${app}-${major_version}.${minor_version}.${build_version}.zip . -x *.git* -x build.sh -x README.md -x LICENSE -x *.zip
+
+# Increment the build version and update the manifest.
+new_build_version=$(( ${build_version} + 1 ))
+sed -i "s/build_version=${build_version}/build_version=${new_build_version}/" manifest
